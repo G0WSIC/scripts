0887948474f6f87259681021adaba6ed8194b8c4
G0WSIC <53444046+G0WSIC@users.noreply.github.com>
2019-07-29 13:44:45 -0400

    Add files via upload

---
+++ AutomoxUninstall.cmd
@@ -0,0 +1,12 @@
+﻿IF EXIST "C:\Program Files (x86)\Automox\amagent.exe" GOTO UNINSTALL
+GOTO END
+:UNINSTALL
+"C:\Program Files (x86)\Automox\amagent.exe" --deregister
+"C:\Program Files (x86)\Automox\unins000.exe" /VERYSILENT /SUPPRESSMSGBOXES
+cd \
+sc stop amagent
+timeout /t 120 /nobreak
+sc delete amagent
+timeout /t 120 /nobreak
+rmdir /s /q "C:\Program Files (x86)\Automox"
+:END
